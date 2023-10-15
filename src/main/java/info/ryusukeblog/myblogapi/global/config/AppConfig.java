be10e1688c4e60a2d3dae5ee80067496ebb9f98d
ryusuke.arata <ryusuke.arata1130@gmail.com>
2023-10-15 17:10:12 +0900

    feat: AppConfigにメッセージをmessageSourceから配信するための設定を追加
    yamlファイルをpropertiesファイルに変換し、変換したpropertiesファイルをmessageSourceとして配信するように設定

--- src/main/java/info/ryusukeblog/myblogapi/global/config/AppConfig.java
+++ src/main/java/info/ryusukeblog/myblogapi/global/config/AppConfig.java
@@ -1,8 +1,14 @@
 package info.ryusukeblog.myblogapi.global.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+
+import java.util.Properties;
 
 @Configuration
 public class AppConfig {
@@ -12,4 +18,17 @@ public class AppConfig {
         return new ModelMapper();
     }
 
+    @Bean(name = "messagesProperties")
+    public Properties yamlProperties() {
+        YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
+        bean.setResources(new ClassPathResource("messages.yml"));
+        return bean.getObject();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setCommonMessages(this.yamlProperties());
+        return messageSource;
+    }
 }
