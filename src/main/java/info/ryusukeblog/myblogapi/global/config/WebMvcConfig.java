563c2531665157ac19936ac90d3cabcee2c097c5
ryusuke.arata <ryusuke.arata1130@gmail.com>
2023-10-15 17:11:20 +0900

    fix: 不要な設定を削除

--- src/main/java/info/ryusukeblog/myblogapi/global/config/WebMvcConfig.java
+++ src/main/java/info/ryusukeblog/myblogapi/global/config/WebMvcConfig.java
@@ -1,21 +1,8 @@
 package info.ryusukeblog.myblogapi.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Value("${ALLOWED_ORIGINS}")
-    private String ALLOWED_ORIGINS;
-
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(ALLOWED_ORIGINS)
-                .allowedMethods("POST", "PATCH", "DELETE");
-    }
 }
