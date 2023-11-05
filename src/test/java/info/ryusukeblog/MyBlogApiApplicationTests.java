f9d96447fa1c2b7ac1c7e0f4862d8ff1b723d3c3
ryusuke.arata <ryusuke.arata1130@gmail.com>
2023-11-05 17:18:11 +0900

    test: 不要なテストを削除

--- src/test/java/info/ryusukeblog/MyBlogApiApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package info.ryusukeblog;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MyBlogApiApplicationTests {
-
-}
